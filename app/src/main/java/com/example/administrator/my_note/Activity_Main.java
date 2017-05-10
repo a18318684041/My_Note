80af81b803d8f9ef008297f6d59218ee62f599e6
Administrator <1144325644@qq.com>
2017-05-10 16:21:02 +0800

    图文编辑，分享等等功能

--- app/src/main/java/com/example/administrator/my_note/Activity_Main.java
+++ app/src/main/java/com/example/administrator/my_note/Activity_Main.java
@@ -4,14 +4,17 @@ import android.app.Activity;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.format.DateFormat;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
@@ -19,9 +22,16 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
+import java.util.Locale;
 
 import jp.wasabeef.richeditor.RichEditor;
 
@@ -44,6 +54,7 @@ public class Activity_Main extends AppCompatActivity {
 
     private LinearLayout photo;
     private LinearLayout take_photo;
+    private String path;//图片的地址
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,7 +66,7 @@ public class Activity_Main extends AppCompatActivity {
     private void initView() {
         photo = (LinearLayout) findViewById(R.id.photo);
         take_photo = (LinearLayout) findViewById(R.id.take_photo);
-        db = openOrCreateDatabase("NoteBook",MODE_PRIVATE,null);
+        db = openOrCreateDatabase("NoteBook", MODE_PRIVATE, null);
         img_back = (ImageView) findViewById(R.id.btn_back);
         tv_time = (TextView) findViewById(R.id.time);
         //edt_content = (EditText) findViewById(R.id.edt_content);
@@ -77,9 +88,17 @@ public class Activity_Main extends AppCompatActivity {
         photo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //调用系统的照相机
+                //使用图片选择器
                 Intent intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
                 startActivityForResult(intent, 1001);
+            }
+        });
+        take_photo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //调用系统的照相机
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, 1002);
             }
         });
 
@@ -106,10 +125,12 @@ public class Activity_Main extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent2 = new Intent(Activity_Main.this, MainActivity.class);
-                //String content1 = edt_content.getText().toString();
                 String time2 = tv_time.getText().toString();
-                db.execSQL("update info set content = '"+HTML+"'where content ='"+content+"'");
-                db.close();
+                //对获取到的HTMl进行判断，如果内容没有改变其值为空
+                if(HTML!=null) {
+                    db.execSQL("update info set content = '" + HTML + "'where content ='" + content + "'");
+                    db.close();
+                }
                 intent2.putExtra("content", content);
                 intent2.putExtra("time", time);
                 startActivity(intent2);
@@ -121,11 +142,20 @@ public class Activity_Main extends AppCompatActivity {
         img_share.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //将记事本中的信息分享到QQ、微信等多媒体
-                Intent intent1=new Intent(Intent.ACTION_SEND);
-                intent1.putExtra(Intent.EXTRA_TEXT,edt_content.getText().toString());
+/*              //将记事本中的信息分享到QQ、微信等多媒体
+                Intent intent1 = new Intent(Intent.ACTION_SEND);
+                intent1.putExtra(Intent.EXTRA_TEXT, edt_content.getText().toString());
                 intent1.setType("text/plain");
-                startActivity(Intent.createChooser(intent1,"share"));
+                startActivity(Intent.createChooser(intent1, "share"));*/
+                //分享图文信息
+                Uri imageUri = Uri.fromFile(new File(path));
+                Intent shareIntent = new Intent();
+                shareIntent.setAction(Intent.ACTION_SEND);
+                shareIntent.putExtra(Intent.EXTRA_STREAM, imageUri);
+                shareIntent.setType("image/*");
+                shareIntent.putExtra(Intent.EXTRA_TEXT, "This is my Share text.");
+                shareIntent.setType("text/plain");
+                startActivity(Intent.createChooser(shareIntent, "分享到"));
             }
         });
     }
@@ -136,7 +166,7 @@ public class Activity_Main extends AppCompatActivity {
             Intent intent2 = new Intent(Activity_Main.this, MainActivity.class);
             //String content1 = edt_content.getText().toString();
             String time2 = tv_time.getText().toString();
-            db.execSQL("update info set content = '"+HTML+"'where content ='"+content+"'");
+            db.execSQL("update info set content = '" + HTML + "'where content ='" + content + "'");
             db.close();
             intent2.putExtra("content", content);
             intent2.putExtra("time", time);
@@ -145,9 +175,10 @@ public class Activity_Main extends AppCompatActivity {
         }
         return super.onKeyDown(keyCode, event);
     }
+
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(data!=null){
+        if (data != null) {
             if (requestCode == 1001 && resultCode == Activity.RESULT_OK) {
                 Uri selectedImage = data.getData();
                 String[] filePathColumn = {MediaStore.Images.Media.DATA};
@@ -163,7 +194,51 @@ public class Activity_Main extends AppCompatActivity {
                 mEditor.requestFocus();
                 mEditor.requestFocusFromTouch();
                 mEditor.insertImage(picturePath, "image");
-                Log.d("AAA", "onActivityResult: "+mEditor.getHtml());
+                Log.d("AAA", "onActivityResult: " + mEditor.getHtml());
+                path = picturePath;
+            }
+            //调用系统照相机的回调
+            else  if(requestCode == 1002 && resultCode == Activity.RESULT_OK){
+                String sdStatus = Environment.getExternalStorageState();
+                if (!sdStatus.equals(Environment.MEDIA_MOUNTED)) { // 检测sd是否可用
+                    Log.i("TestFile",
+                            "SD card is not avaiable/writeable right now.");
+                    return;
+                }
+                new DateFormat();
+                String name = DateFormat.format("yyyyMMdd_hhmmss", Calendar.getInstance(Locale.CHINA)) + ".jpg";
+                Toast.makeText(this, name, Toast.LENGTH_LONG).show();
+                Bundle bundle = data.getExtras();
+                Bitmap bitmap = (Bitmap) bundle.get("data");// 获取相机返回的数据，并转换为Bitmap图片格式
+
+                FileOutputStream b = null;
+                File file = new File("/sdcard/Image/");
+                file.mkdirs();// 创建文件夹
+                String fileName = "/sdcard/Image/"+name;
+
+                try {
+                    b = new FileOutputStream(fileName);
+                    bitmap.compress(Bitmap.CompressFormat.JPEG, 100, b);// 把数据写入文件
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } finally {
+                    try {
+                        b.flush();
+                        b.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                try
+                {
+                    //将图片插入到编辑器中
+                    mEditor.requestFocus();
+                    mEditor.requestFocusFromTouch();
+                    mEditor.insertImage(fileName,"image");
+                }catch(Exception e)
+                {
+                    Log.e("error", e.getMessage());
+                }
             }
         }
     }
--- app/src/main/java/com/example/administrator/my_note/Activity_add.java
+++ app/src/main/java/com/example/administrator/my_note/Activity_add.java
@@ -4,14 +4,17 @@ import android.app.Activity;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.format.DateFormat;
 import android.text.format.Time;
 import android.util.Log;
 import android.view.View;
@@ -22,8 +25,14 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
+import java.util.Locale;
 import java.util.Timer;
 
 import jp.wasabeef.richeditor.RichEditor;
@@ -37,7 +46,7 @@ public class Activity_add extends AppCompatActivity {
     private EditText edt_content;
     SQLiteDatabase db;
 
-    private String  first_time;
+    private String first_time;
     private String last_time;
     private String content;
 
@@ -47,11 +56,13 @@ public class Activity_add extends AppCompatActivity {
     private List<String> contents = new ArrayList<>();
 
 
+
     //图片选择，拍照，分享
     private LinearLayout photo;
     private LinearLayout take_photo;
-    private  LinearLayout share;
+    private LinearLayout share;
     private ImageView img_share;
+    private String path;//图片的地址
 
 
     //图片文字编辑框
@@ -76,25 +87,29 @@ public class Activity_add extends AppCompatActivity {
         img_share.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //将记事本中的信息分享到QQ、微信等多媒体
-                Intent intent1=new Intent(Intent.ACTION_SEND);
-                intent1.putExtra(Intent.EXTRA_TEXT,edt_content.getText().toString());
-                intent1.setType("text/plain");
-                startActivity(Intent.createChooser(intent1,"share"));
+                Uri imageUri = Uri.fromFile(new File(path));
+                Intent shareIntent = new Intent();
+                shareIntent.setAction(Intent.ACTION_SEND);
+                shareIntent.putExtra(Intent.EXTRA_STREAM, imageUri);
+                shareIntent.setType("image/*");
+                shareIntent.putExtra(Intent.EXTRA_TEXT, "This is my Share text.");
+                shareIntent.setType("text/plain");
+                startActivity(Intent.createChooser(shareIntent, "分享到"));
             }
         });
         take_photo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //调用系统的照相机
                 Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(intent, 1);
+                startActivityForResult(intent, 1002);
             }
         });
         //选择图片
         photo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //调用系统的照相机
+                //调用系统的图片选择器
                 Intent intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
                 startActivityForResult(intent, 1001);
             }
@@ -104,7 +119,7 @@ public class Activity_add extends AppCompatActivity {
     private void initDatabase() {
         //打开数据库
         //db = openOrCreateDatabase("MyNote",MODE_PRIVATE,null);
-        db = openOrCreateDatabase("NoteBook",MODE_PRIVATE,null);
+        db = openOrCreateDatabase("NoteBook", MODE_PRIVATE, null);
     }
 
     private void initView() {
@@ -148,16 +163,17 @@ public class Activity_add extends AppCompatActivity {
                 int hour = time.hour;
                 int minute = time.minute;
                 int second = time.second;
-                last_time = (month+1) + "月"+day+"日";
+                last_time = (month + 1) + "月" + day + "日";
                 //获取到最后修改的时间
                 //content = edt_content.getText().toString();//获取到便签的内容
-                Intent intent = new Intent(Activity_add.this,MainActivity.class);
-                db.execSQL("insert into info(first_time,last_time,content) values('"+first_time+"','"+last_time+"','"+HTML+"')");
+                Intent intent = new Intent(Activity_add.this, MainActivity.class);
+                db.execSQL("insert into info(first_time,last_time,content) values('" + first_time + "','" + last_time + "','" + HTML + "')");
                 db.close();
                 startActivity(intent);
             }
         });
     }
+
     private void NowTime() {
         Time time = new Time("GMT+8");
         time.setToNow();
@@ -167,14 +183,15 @@ public class Activity_add extends AppCompatActivity {
         int hour = time.hour;
         int minute = time.minute;
         int second = time.second;
-        tv_time.setText(year+"-"+(month+1)+"-"+day);
-        first_time = year+"-"+(month+1)+"-"+day;
+        tv_time.setText(year + "-" + (month + 1) + "-" + day);
+        first_time = year + "-" + (month + 1) + "-" + day;
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(data!=null){
+        if (data != null) {
+            //图片选择器
             if (requestCode == 1001 && resultCode == Activity.RESULT_OK) {
                 Uri selectedImage = data.getData();
                 String[] filePathColumn = {MediaStore.Images.Media.DATA};
@@ -186,11 +203,52 @@ public class Activity_add extends AppCompatActivity {
                 final String picturePath = cursor.getString(columnIndex);
                 cursor.close();
                 Log.d("AAA", picturePath);
+                path = picturePath;
                 //自动获取输入焦点
                 mEditor.requestFocus();
                 mEditor.requestFocusFromTouch();
                 mEditor.insertImage(picturePath, "image");
-                Log.d("AAA", "onActivityResult: "+mEditor.getHtml());
+                Log.d("AAA", "onActivityResult: " + mEditor.getHtml());
+            }//调用系统照相机的回调
+            else if (requestCode == 1002 && resultCode == Activity.RESULT_OK) {
+                String sdStatus = Environment.getExternalStorageState();
+                if (!sdStatus.equals(Environment.MEDIA_MOUNTED)) { // 检测sd是否可用
+                    Log.i("TestFile",
+                            "SD card is not avaiable/writeable right now.");
+                    return;
+                }
+                new DateFormat();
+                String name = DateFormat.format("yyyyMMdd_hhmmss", Calendar.getInstance(Locale.CHINA)) + ".jpg";
+                Toast.makeText(this, name, Toast.LENGTH_LONG).show();
+                Bundle bundle = data.getExtras();
+                Bitmap bitmap = (Bitmap) bundle.get("data");// 获取相机返回的数据，并转换为Bitmap图片格式
+
+                FileOutputStream b = null;
+                File file = new File("/sdcard/Image/");
+                file.mkdirs();// 创建文件夹
+                String fileName = "/sdcard/Image/" + name;
+
+                try {
+                    b = new FileOutputStream(fileName);
+                    bitmap.compress(Bitmap.CompressFormat.JPEG, 100, b);// 把数据写入文件
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } finally {
+                    try {
+                        b.flush();
+                        b.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                try {
+                    //将图片插入到编辑器中
+                    mEditor.requestFocus();
+                    mEditor.requestFocusFromTouch();
+                    mEditor.insertImage(fileName, "image");
+                } catch (Exception e) {
+                    Log.e("error", e.getMessage());
+                }
             }
         }
     }
